b62dfeb2e72bf2bbe76edd2842fc842db7b17a3d
rahimlisarkhan <rahimlisarkhan@gmail.com>
2021-12-22 15:30:30 +0400

    update button

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,18 +2,18 @@ import { Fragment, useState } from "react"
 import { ItemType } from "../../interfaces"
 
 type MyProps = {
-    item?:ItemType
+    item?: ItemType
 }
 
 
-export const Card:React.FC<MyProps> = ({item:{name,description,image}}) => {
+export const Card: React.FC<MyProps> = ({ item: { name, description, image } }) => {
 
     let [read, setRead] = useState(false)
 
     let showImage = () => {
         let imgUrl = 'https://roofat.az/wp-content/themes/consultix/images/no-image-found-360x260.png'
-        
-        if(image){
+
+        if (image) {
             imgUrl = image
         }
         return imgUrl
@@ -21,18 +21,18 @@ export const Card:React.FC<MyProps> = ({item:{name,description,image}}) => {
 
     return (
         <Fragment>
-            <div className="mb-5">
+            <div className="mb-5 " style={{ position: "relative" }}>
                 <div className="card" >
-                    <img src={showImage()} style={{height:250}} className="card-img-top" alt={name} />
+                    <img src={showImage()} style={{ height: 250 }} className="card-img-top" alt={name} />
                     <div className="card-body">
                         <h5 className="card-title">{name}</h5>
                         {!read && <button onClick={() => setRead(true)} className="btn btn-success">read more</button>}
                     </div>
                 </div>
                 {read &&
-                    <div className="jumbotron p-3 bg-success text-center text-white">
+                    <div className="jumbotron p-3 bg-success text-center text-white " style={{ position: "absolute", top: 320,zIndex:2 }}>
                         <p className="lead">{description}</p>
-                        <button  className="btn btn-outline-light" onClick={() => setRead(false)}>HIDE</button>
+                        <button className="btn btn-outline-light" onClick={() => setRead(false)}>HIDE</button>
                     </div>
                 }
             </div>
